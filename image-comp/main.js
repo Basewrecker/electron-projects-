fbff7c1f626393f39c5dc234a0f70765b41f50d1
Basewrecker <siddhunrs@gmail.com>
2026-05-06 20:11:48 +0530

    Image comp initial setup

---
+++ image-comp/main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "ImageComp",
+    width: 500,
+    height: 600,
+    icon: '${__dirname}/assets/icons/Icon_256x256.png',
+  });
+
+  
+  mainWindow.loadFile('./app/index.html');
+}
+
+app.on("ready", createMainWindow);
