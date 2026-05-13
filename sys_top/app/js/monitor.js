b6272006dcaf02e278ddbcc2f2f876bb01d25916
Basewrecker <siddhunrs@gmail.com>
2026-05-13 12:48:57 +0530

    Added store.js for user settings and made it a func var

--- sys_top/app/js/monitor.js
+++ sys_top/app/js/monitor.js
@@ -1,59 +1,94 @@
-const path = require('path');
-const nodeOs = require('os');
-const { CPUMonitor, MemoryMonitor, SystemMonitor } = require('node-os-utils');
-const cpu = new CPUMonitor()
-const mem = new MemoryMonitor()
-const os = new SystemMonitor()
+const path = require("path");
+const nodeOs = require("os");
+const { CPUMonitor, MemoryMonitor, SystemMonitor } = require("node-os-utils");
+const cpu = new CPUMonitor();
+const mem = new MemoryMonitor();
+const os = new SystemMonitor();
 
-let cpuOverload = 5;
+let cpuOverload = 85;
+let alertFrequency = 1;
 
 function getCpuUsage() {
-    return new Promise((resolve) => {
-        const cpus1 = nodeOs.cpus()
-        setTimeout(() => {
-            const cpus2 = nodeOs.cpus()
-            let totalIdle = 0, totalTick = 0
-            cpus1.forEach((_, i) => {
-                const t1 = cpus1[i].times
-                const t2 = cpus2[i].times
-                for (const type in t2) totalTick += t2[type] - t1[type]
-                totalIdle += t2.idle - t1.idle
-            })
-            resolve((100 - (100 * totalIdle / totalTick)).toFixed(1))
-        }, 200)
-    })
+  return new Promise((resolve) => {
+    const cpus1 = nodeOs.cpus();
+    setTimeout(() => {
+      const cpus2 = nodeOs.cpus();
+      let totalIdle = 0,
+        totalTick = 0;
+      cpus1.forEach((_, i) => {
+        const t1 = cpus1[i].times;
+        const t2 = cpus2[i].times;
+        for (const type in t2) totalTick += t2[type] - t1[type];
+        totalIdle += t2.idle - t1.idle;
+      });
+      resolve((100 - (100 * totalIdle) / totalTick).toFixed(1));
+    }, 200);
+  });
 }
 
 // Run every 1.5 seconds
 setInterval(() => {
-    getCpuUsage().then(usage => {
-        document.getElementById('cpu-usage').innerText = usage + '%'
-        document.getElementById('cpu-free').innerText = (100 - usage).toFixed(1) + '%'
-        document.getElementById('cpu-progress').style.width = usage + '%'
-        if (usage > overload) {
-           document.getElementById('cpu-progress').style.background = 'red'    
-        } else {
-           document.getElementById('cpu-progress').style.background = '#30c88b'
-        }
-    })
+  getCpuUsage().then((usage) => {
+    document.getElementById("cpu-usage").innerText = usage + "%";
+    document.getElementById("cpu-free").innerText =
+      (100 - usage).toFixed(1) + "%";
+    document.getElementById("cpu-progress").style.width = usage + "%";
+    if (usage > cpuOverload) {
+      document.getElementById("cpu-progress").style.background = "red";
+    } else {
+      document.getElementById("cpu-progress").style.background = "#30c88b";
+    }
+  });
+
+  // Check overload
+  if (info >= cpuOverload && runNotify(alertFrequency)) {
+    notifyUser({
+      title: "CPU overload",
+      body: `Cpu is over ${cpuOverload}`,
+      icon: path.join(__dirname, "img", "./icon.png"),
+    });
+
+    localStorage.setItem("lastNotify", +new Date());
+  }
 }, 1500);
-
-
 
 // Format uptime date time format
 
 function formatUptime(seconds) {
-    const days = Math.floor(seconds / 86400);
-    const hours = Math.floor((seconds % 86400) / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const second = Math.floor(seconds % 60);
+  const days = Math.floor(seconds / 86400);
+  const hours = Math.floor((seconds % 86400) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const second = Math.floor(seconds % 60);
 
-    return `${days}d ${hours}h ${minutes}m ${second}s`;
+  return `${days}d ${hours}h ${minutes}m ${second}s`;
 }
 
+document.getElementById("cpu-model").innerText = cpu.model();
+document.getElementById("comp-name").innerText = nodeOs.hostname();
+document.getElementById("os").innerText = `${nodeOs.type()} ${nodeOs.arch()}`;
+document.getElementById("sys-uptime").innerText = formatUptime(nodeOs.uptime());
+document.getElementById("mem-total").innerText =
+  (nodeOs.totalmem() / 1024 / 1024 / 1024).toFixed(1) + " GB";
 
-document.getElementById('cpu-model').innerText = cpu.model()
-document.getElementById('comp-name').innerText = nodeOs.hostname()
-document.getElementById('os').innerText = `${nodeOs.type()} ${nodeOs.arch()}`
-document.getElementById('sys-uptime').innerText = formatUptime(nodeOs.uptime())
-document.getElementById('mem-total').innerText = (nodeOs.totalmem() / 1024 / 1024 / 1024).toFixed(1) + ' GB'
+// Send notification
+
+function notifyUser(options) {
+  new Notification(options.title, options);
+}
+
+function runNotify(frequency) {
+  if (localStorage.getItem("lastNotify") === null) {
+    localStorage.setItem("lastNotify", +new Date());
+    return true;
+  }
+  const notifyTime = new Date(parseInt(localStorage.getItem("lastNotify")));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPassed = Math.ceil(diffTime / (1000 * 60));
+
+  if (minutesPassed > frequency) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- sys_top/main.js
+++ sys_top/main.js
@@ -1,71 +1,81 @@
-const { app, BrowserWindow, Menu } = require('electron')
-const log = require('electron-log')
+const { app, BrowserWindow, Menu } = require("electron");
+const log = require("electron-log");
+const Store = require("./Store");
 
 // Set env
-process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = "development";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false
-const isMac = process.platform === 'darwin' ? true : false
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
-let mainWindow
+let mainWindow;
+const store = new Store({
+  configName: "user-settings",
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+});
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    title: 'SysTop',
+    title: "SysTop",
     width: isDev ? 700 : 355,
     height: 600,
-    icon: './assets/icons/icon.png',
+    icon: "./assets/icons/icon.png",
     resizable: isDev ? true : false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
-  })
+  });
 
   if (isDev) {
-    mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools();
   }
 
-  mainWindow.loadFile('./app/index.html')
+  mainWindow.loadFile("./app/index.html");
 }
 
-app.on('ready', () => {
-  createMainWindow()
+app.on("ready", () => {
+  createMainWindow();
 
-  const mainMenu = Menu.buildFromTemplate(menu)
-  Menu.setApplicationMenu(mainMenu)
-})
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+});
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
+  ...(isMac ? [{ role: "appMenu" }] : []),
   {
-    role: 'fileMenu',
+    role: "fileMenu",
   },
   ...(isDev
     ? [
         {
-          label: 'Developer',
+          label: "Developer",
           submenu: [
-            { role: 'reload' },
-            { role: 'forcereload' },
-            { type: 'separator' },
-            { role: 'toggledevtools' },
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
           ],
         },
       ]
     : []),
-]
+];
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!isMac) {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
+    createMainWindow();
   }
-})
+});
 
-app.allowRendererProcessReuse = true
+app.allowRendererProcessReuse = true;
